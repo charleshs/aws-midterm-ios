4ffffba4884324e5f66b68da9399cbe76371b335
charleshs <charlous167@gmail.com>
2020-01-17 09:19:32 +0800

    Create Xcode project.

---
+++ Midterm-iOS/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Midterm-iOS
+//
+//  Created by Kai-Ta Hsieh on 2020/1/17.
+//  Copyright © 2020 Kai-Ta Hsieh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+  
+}
+
